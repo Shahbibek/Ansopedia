cabd2de4395e6aeb506e488a6f8b741bf53fecd3
Bibek Sah <bkumarsah627@rku.ac.in>
2022-12-15 00:16:02 +0530

    Contat us updated

--- app/src/main/java/com/example/ansopedia/MyProfileActivity.java
+++ app/src/main/java/com/example/ansopedia/MyProfileActivity.java
@@ -2,13 +2,50 @@ package com.example.ansopedia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.ansopedia.Model.SharedPrefManager;
+import com.example.ansopedia.Model.UserModel;
 
 public class MyProfileActivity extends AppCompatActivity {
-
+    private TextView tvUserName, tvUserEmail;
+    private Button btnLogout;
+    private LinearLayout llEditSetting;
+    UserModel userModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
+        intiViews();
+        userModel = SharedPrefManager.getInstance(this).getUser();
+        tvUserName.setText(userModel.getUserName());
+        tvUserEmail.setText(userModel.getEmail());
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPrefManager.getInstance(MyProfileActivity.this).logout();
+            }
+        });
+
+        llEditSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MyProfileActivity.this, EditProfileActivity.class));
+                finish();
+            }
+        });
+    }
+
+    private void intiViews() {
+        tvUserName = findViewById(R.id.userName);
+        tvUserEmail = findViewById(R.id.userEmail);
+        btnLogout = findViewById(R.id.logoutButton);
+        llEditSetting = findViewById(R.id.llEditSetting);
     }
 }
