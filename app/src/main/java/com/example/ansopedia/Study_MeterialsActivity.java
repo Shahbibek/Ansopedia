43ac19f1278c4618f8cecba4d851ffbfeeb5bae8
Bibek Sah <bkumarsah627@rku.ac.in>
2022-12-17 11:58:57 +0530

    study metarials added

---
+++ app/src/main/java/com/example/ansopedia/Study_MeterialsActivity.java
@@ -0,0 +1,14 @@
+package com.example.ansopedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Study_MeterialsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study_meterials);
+    }
+}
